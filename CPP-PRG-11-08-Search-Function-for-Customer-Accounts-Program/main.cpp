29ec6ad12a4651d36b59eaae0679f5275c60db84
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-08 17:09:41 -0500

    Prompt written.

--- CPP-PRG-11-08-Search-Function-for-Customer-Accounts-Program/main.cpp
+++ CPP-PRG-11-08-Search-Function-for-Customer-Accounts-Program/main.cpp
@@ -5,11 +5,19 @@
 //  Created by Keith Smith on 11/8/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Add a function to Programming Challenge 7 (Customer Accounts) that allows the user
+//  to search the structure array for a particular customer's account. It should accept part
+//  of the customer's name as an argument then search for an account with a name that
+//  matches it. All accounts that match should be displayed. If no account matches, a
+//  message saying so should be displayed.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
+    
     return 0;
 }
